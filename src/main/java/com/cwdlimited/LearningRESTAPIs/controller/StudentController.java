11932d1e60856aed36f48f4c585320731adc133b
Shahnawaz Khan <shahnawaz.khan@cwdin.com>
2026-04-29 12:55:26 +0530

    Making repository interface

--- src/main/java/com/cwdlimited/LearningRESTAPIs/controller/StudentController.java
+++ src/main/java/com/cwdlimited/LearningRESTAPIs/controller/StudentController.java
@@ -1,14 +1,29 @@
 package com.cwdlimited.LearningRESTAPIs.controller;
 
 import com.cwdlimited.LearningRESTAPIs.dto.StudentDto;
+import com.cwdlimited.LearningRESTAPIs.entity.Student;
+import com.cwdlimited.repository.StudentRepository;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
+    private final StudentRepository studentRepository;
+    
+    public StudentController(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDto getStudent() {
+    public List<Student> getStudent() {
+        return studentRepository.findAll();
+    }
+    @GetMapping("/student/{id}")
+    public StudentDto getStudentById(){
         return new StudentDto(18L,"Virat","virat.kohli@gmail.com");
     }
 }
---
+++ src/main/java/com/cwdlimited/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.cwdlimited.repository;
+
+import com.cwdlimited.LearningRESTAPIs.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
