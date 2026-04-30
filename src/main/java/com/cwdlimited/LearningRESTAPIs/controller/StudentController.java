86c495a6c70b1a42a5f6b074c4cd7267673c0091
Shahnawaz Khan <shahnawaz.khan@cwdin.com>
2026-04-30 15:38:21 +0530

    added a delete database operation

--- src/main/java/com/cwdlimited/LearningRESTAPIs/controller/StudentController.java
+++ src/main/java/com/cwdlimited/LearningRESTAPIs/controller/StudentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,11 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<StudentDto> createNewStudent(@RequestBody AddStudentRequestDto addStudentRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createNewStudent(addStudentRequestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAStudent(@PathVariable Long id){
+        studentService.deleteStudentById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/cwdlimited/LearningRESTAPIs/service/impl/StudentServiceImpl.java
+++ src/main/java/com/cwdlimited/LearningRESTAPIs/service/impl/StudentServiceImpl.java
@@ -42,4 +42,12 @@ public class StudentServiceImpl implements StudentService {
         Student student = studentRepository.save(newStudent);
         return modelMapper.map(student, StudentDto.class);
     }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        if(!studentRepository.existsById(id)){
+            throw new IllegalArgumentException("Student Does Not Exists By ID: " + id);
+        }
+        studentRepository.deleteById(id);
+    }
 }
